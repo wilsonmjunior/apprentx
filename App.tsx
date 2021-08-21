f702caf5ce0aa3a79a427bde0b87a8fdd35b00fa
wilson junior <wilsonmjuniordx@gmail.com>
2021-08-21 20:54:29 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+import { 
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo'
+import AppLoading from 'expo-app-loading';
+
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  })
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading />
+    )
+  }
+  
+  return (
+    <Home />
+  );
+}
